cdcb4e689e0e4eb808ffc36bb3e1a4cbd13bc84a
Tyuiu.DragomeretskiyED@outlook.com <mrtvinksi@mail.ru>
2023-11-05 17:55:01 +0500

    Task1

---
+++ Tyuiu.DragomeretskiyED.Sprint4.Task1.V21/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.DragomeretskiyED.Sprint4.Task1.V21.Lib;
+
+namespace Tyuiu.DragomeretskiyED.Sprint4.Task1.V21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Драгомерецкий Е.Д | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнила: Драгомерецкий Егор Дмитриевич | СМАРТб-23-1                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Подсчитать произведение четных элементов массива.                       *");
+            Console.WriteLine("* { 2, 4, 8, 5, 2, 6, 5, 8, 8, 6, 6 }                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int[] array = new int[11];
+
+
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                Console.WriteLine("Введите значение " + i + " элемента массива");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                Console.WriteLine(array[i] + "\t");
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(array);
+            Console.WriteLine("Результат: " + res);
+
+            Console.ReadKey();
+
+        }
+    }
+}
