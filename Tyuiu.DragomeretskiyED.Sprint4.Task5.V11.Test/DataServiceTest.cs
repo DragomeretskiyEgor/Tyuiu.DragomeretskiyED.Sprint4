c4d7d6436868350c7b9874ec03d8a103afb2f453
Tyuiu.DragomeretskiyED@outlook.com <mrtvinksi@mail.ru>
2023-11-12 16:17:55 +0500

    Task5

---
+++ Tyuiu.DragomeretskiyED.Sprint4.Task5.V11.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.DragomeretskiyED.Sprint4.Task5.V11.Lib;
+
+namespace Tyuiu.DragomeretskiyED.Sprint4.Task5.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Calculate_Test()
+        {
+            int[,] mtrx = new int[2, 2] { {-5, 2 },
+                                          { 2, 1} };
+            int[,] res = new int[2, 2] { {-5, 1 },
+                                         { 1, 1} };
+            DataService ds = new DataService();
+            CollectionAssert.AreEqual(ds.Calculate(mtrx), res);
+        }
+    }
+}
---
+++ Tyuiu.DragomeretskiyED.Sprint4.Task5.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DragomeretskiyED.Sprint4.Task5.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DragomeretskiyED.Sprint4.Task5.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2487ce96-9a67-4070-9232-54c64376476b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
